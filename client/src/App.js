a4db0ce1b463bc4293b9463c5b58617e2ae665bc
Yuya Satake <sata.yu.7878@gmail.com>
2022-07-12 12:32:26 -0700

    tailwind init

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-
+import "./index.css";
 
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+     </h1>
   );
 }
 
