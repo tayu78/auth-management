281e4c3c0c9aef94b28bbfab8711377e0aa29a77
Yuya Satake <sata.yu.7878@gmail.com>
2022-09-27 21:11:24 -0700

    crated hamburger menu

--- client/src/common/Header/Header.jsx
+++ client/src/common/Header/Header.jsx
@@ -1,38 +1,60 @@
-import React, {useState} from 'react';
-import { useNavigate, Link } from 'react-router-dom';
-import { useUserContext } from '../../hooks/useUserContext';
+import React, { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { useUserContext } from "../../hooks/useUserContext";
 import "./Header.css";
 
 const Header = () => {
   const { user, setUser } = useUserContext();
   const navigate = useNavigate();
-
-  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
   const signOut = () => {
     setUser(null);
     navigate("/signin");
-  }
-  
-  
+  };
+
+  const handleIsActive = () => {
+    setIsActive((prev) => !prev);
+  };
+
   return (
-    <div className='flex items-center justify-between  bg-slate-800 h-16  text-white px-3 fixed top-0 left-0 w-full z-50 '  >
-      <Link className='font-bold' to={"/"}>KENGENKANRI</Link> 
-      {/* {user && 
-        <nav className='flex items-center justify-around w-1/3'>
-        <Link to="/">Home</Link>
-        <Link to="userManagement">User</Link>
-        <Link to="roleManagement">Role</Link>
-        <Link to="permissionManagement">Permission</Link>
-       <button onClick={signOut}>Logout</button>
-      </nav>
-      } */}
-      <div className='burger-menu'>
-        <div className='burger-line'></div>
-      </div>
-      
+    <div className="flex items-center justify-between  bg-slate-800 h-16  text-white px-3 fixed top-0 left-0 w-full z-50 ">
+      <Link className="font-bold" to={"/"}>
+        KENGENKANRI
+      </Link>
+      {user && (
+        <>
+          <nav
+            className={`primary-nav ${
+              isActive ? "active" : ""
+            } flex items-center justify-around w-1/3"`}
+          >
+            <Link onClick={handleIsActive} to="/">
+              Home
+            </Link>
+            <Link onClick={handleIsActive} to="userManagement">
+              User
+            </Link>
+            <Link onClick={handleIsActive} to="roleManagement">
+              Role
+            </Link>
+            <Link onClick={handleIsActive} to="permissionManagement">
+              Permission
+            </Link>
+            <button onClick={signOut}>Logout</button>
+          </nav>
+          <div
+            className={`burger-menu ${isActive ? "active" : ""}`}
+            onClick={handleIsActive}
+          >
+            <span className="burger-bar"></span>
+            <span className="burger-bar"></span>
+            <span className="burger-bar"></span>
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Header;
